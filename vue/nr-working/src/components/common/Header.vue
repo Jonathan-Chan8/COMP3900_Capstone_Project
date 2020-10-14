397d8bdbd73b746018e45603d99837c66abde018
Liam Dale <liam.dale2@gmail.com>
2020-10-15 10:59:09 +1100

    basic auth

--- vue/nr-working/src/components/common/Header.vue
+++ vue/nr-working/src/components/common/Header.vue
@@ -10,30 +10,48 @@
             </v-toolbar-title>
         </v-btn>
         <v-spacer></v-spacer>
+        <!-- Need to properly implement the login authorisation methods once that story is completed -->
         <v-toolbar-items class="d-none d-md-block">
             <v-btn text v-for="item in menu" :key="item.icon" :to="item.route" depressed>{{ item.title }}
             </v-btn>
-            <v-menu offset-y>
-                <template v-slot:activator="{ on }">
+
+            <!-- Only shown if not logged in -->
+            <template v-if="!auth">
+                <v-btn text v-for="item in unreg_links" :key="item.icon" :to="item.route" depressed>{{ item.title }}
+                </v-btn>
+            </template>
+
+            <!-- Only shown if logged in -->
+            <v-menu v-if="auth" offset-y>
+                <template v-slot:activator=" { on }">
                     <v-btn text v-on="on">
                         <span>My Account</span>
                     </v-btn>
                 </template>
                 <v-list class="responsiveMenu">
-                    <v-list-item text v-for="item in unreg_links" :key="item.icon" :to="item.route" depressed>{{ item.title }}
+                    <v-list-item text v-for="item in reg_links" :key="item.icon" :to="item.route" depressed>{{ item.title }}
                     </v-list-item>
                 </v-list>
             </v-menu>
         </v-toolbar-items>
+
         <div class="d-md-none">
             <v-menu>
                 <template v-slot:activator="{ on }">
                     <v-app-bar-nav-icon v-on="on"></v-app-bar-nav-icon>
                 </template>
                 <v-list class="responsiveMenu">
+                    <!-- Always shown -->
+
                     <v-list-item text v-for="item in menu" :key="item.icon" :to="item.route" depressed>{{ item.title }}
                     </v-list-item>
+
+                    <!-- Only shown if not logged in -->
                     <v-list-item text v-for="item in unreg_links" :key="item.icon" :to="item.route" depressed>{{ item.title }}
+                    </v-list-item>
+
+                    <!-- Only shown if logged in -->
+                    <v-list-item text v-for="item in reg_links" :key="item.icon" :to="item.route" depressed>{{ item.title }}
                     </v-list-item>
                 </v-list>
             </v-menu>
@@ -47,6 +65,7 @@
 export default {
     name: "Header",
     data: () => ({
+        auth: true,
         drawer: false,
         unreg_links: [{
                 icon: 'login',
@@ -61,7 +80,7 @@ export default {
         ],
         reg_links: [{
                 icon: 'mytrends',
-                title: 'My Trends',
+                title: 'My Saved Trends',
                 route: '/mytrends'
             },
             {
--- vue/nr-working/src/views/Home.vue
+++ vue/nr-working/src/views/Home.vue
@@ -5,26 +5,35 @@
     <v-container fluid>
         <v-row justify=“center” align=“center”>
             <v-col v-for="card in cards" :key="card.id" cols='auto' md='6'>
-                <v-card v-if='card.id==="unreg"' class="pb-0 flex-wrap text-justify justify-space-between" rounded height="100%" hover>
-                    <v-card-title class="headline" v-text="card.title"></v-card-title>
-                    <v-card-text> {{ card.text }} </v-card-text>
-
-                    <v-card-actions>
-                        <v-btn depressed width=49% large to='/register'>Register</v-btn>
-                        <v-btn depressed width=49% large class="ml-auto" to='login'>Log In</v-btn>
-                    </v-card-actions>
-
-                </v-card>
-                <v-card v-else-if='card.id==="totd"' class="pb-0 flex-wrap text-justify justify-space-between" rounded height="100%" hover>
-                    <v-card-title class="headline" v-text="card.title"></v-card-title>
-                    <v-card-text> {{ card.text }} </v-card-text>
-
-                </v-card>
-                <v-card v-else class="flex-wrap text-justify justify-space-between" rounded height="100%" hover :to='card.route'>
+                <v-card class="flex-wrap text-justify justify-space-between" rounded height="100%" hover :to='card.route'>
                     <v-card-title class="headline" v-text="card.title"></v-card-title>
                     <v-card-text> {{ card.text }} </v-card-text>
                 </v-card>
             </v-col>
+
+            <template v-if=auth>
+                <v-col v-for="card in reg_cards" :key="card.id" cols='auto' md='6'>
+                    <v-card class="flex-wrap text-justify justify-space-between" rounded height="100%" hover :to='card.route'>
+                        <v-card-title class="headline" v-text="card.title"></v-card-title>
+                        <v-card-text> {{ card.text }} </v-card-text>
+                    </v-card>
+                </v-col>
+            </template>
+
+            <template v-if=!auth>
+                <v-col v-for="card in unreg_cards" :key="card.id" cols='auto' md='6'>
+                    <v-card class="pb-0 flex-wrap text-justify justify-space-between" rounded height="100%" hover>
+                        <v-card-title class="headline" v-text="card.title"></v-card-title>
+                        <v-card-text> {{ card.text }} </v-card-text>
+
+                        <v-card-actions>
+                            <v-btn depressed width=49% large to='/register'>Register</v-btn>
+                            <v-btn depressed width=49% large class="ml-auto" to='login'>Log In</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-col>
+            </template>
+
         </v-row>
     </v-container>
 
@@ -37,36 +46,40 @@
 export default {
     data() {
         return {
+            auth: true,
+
             absolute: true,
             opacity: 10,
             overlay: false,
+            unreg_cards: [{
+                id: 'unreg',
+                title: 'Register or Log In',
+                text: ' Log in or register an account to get access to additional features, such as selecting more topics on your Trends graph and saving topics for later!',
+                route: '/register'
+            }, ],
+            reg_cards: [{
+
+                id: 'saved',
+                title: 'My Saved Trends',
+                text: 'Click here to view your saved trends, and explore how your selected topics are behaving!',
+                route: '/saved'
+            }, ],
             cards: [{
                     id: 'totd',
-                    title: "Today's Topic",
+                    title: "Topic of the Day",
                     text: 'We will have here some function to request the most popular topic in the last 24 hours, and clicking it will open the associated overlay. Or, we could simply have this as that topics popup info. Lots of options!',
                     route: ''
                 }, {
-                    id: 'unreg',
-                    title: 'Register or Log In',
-                    text: 'Looking to see how news topics change in relation to each other? Head to the Trends page to view the popularity of selected topics over time on a line graph. You can register an account for additional features, such as saving Trends configurations for later and selecting additional topics.',
-                    route: '/register'
-                }, {
                     id: 'topics',
-                    title: 'Topics',
-                    text: 'Sometimes, you just need to see the big picture. On the Topics page, you can explore the most popular news topics however you want. Adjust the time period, and filter by media outlets, to view which topics are the most prominent in the media landscape.',
+                    title: 'What is Topics?',
+                    text: 'Sometimes, you just need to see the big picture. On the Topics page, you can explore the most popular news topics however you want. Adjust the time period, and filter by media outlets, to view which topics are the most prominent in the media landscape. Click to go to Topics!',
                     route: '/topics'
                 },
                 {
                     id: 'trends',
-                    title: 'Trends',
-                    text: 'Looking to see how news topics change in relation to each other? Head to the Trends page to view the popularity of selected topics over time on a line graph. You can register an account for additional features, such as saving Trends configurations for later and selecting additional topics.',
+                    title: 'What is Trends?',
+                    text: 'Looking to see how news topics change in relation to each other? Head to the Trends page to view the popularity of selected topics over time on a line graph. You can register an account for additional features, such as saving Trends configurations for later and selecting additional topics. Click to go to Trends!',
                     route: '/trends'
-                },
-                {
-                    id: 'saved',
-                    title: 'Saved Trends',
-                    text: 'Click here to view your saved trends, and explore how your selected topics are behaving!',
-                    route: '/saved'
                 }
             ]
 
