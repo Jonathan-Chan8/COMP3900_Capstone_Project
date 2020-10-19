87add51caf83ab353cfb0cd6aa273ebcc4358023
Liam Dale <liam.dale2@gmail.com>
2020-10-19 17:24:25 +1100

    functioning topics page - just requires real data and linking methods/queries

--- vue/nr-working/src/views/Topics.vue
+++ vue/nr-working/src/views/Topics.vue
@@ -8,56 +8,30 @@
         <v-layout wrap>
             <v-spacer />
 
-            <v-flex xs12 md3>
-                <v-text-field prepend-icon="mdi-magnify" label="Search" single-line hide-details @input="filterSearch"> </v-text-field>
+            <v-flex xs10 md3>
+                <!-- Really, these filters wont actually filter the datatable, but rather will be used as input to our db query, thus changing the reuslts of the topics list returned by the database -->
 
-                <v-text-field prepend-icon="mdi-book-open-variant" label="Media Outlet" single-line hide-details @input="filterMedia"></v-text-field>
+                <v-text-field v-model="search" prepend-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field>
 
-                <template>
-                    <v-row>
-                        <v-col>
-                            <v-menu ref="menu" v-model="menu" :close-on-content-click="false" :return-value.sync="date" transition="scale-transition" offset-y min-width="290px">
-                                <template v-slot:activator="{ on, attrs }">
-                                    <v-text-field v-model="dateRangeText" label="Date Range" prepend-icon="mdi-calendar" v-bind="attrs" v-on="on"></v-text-field>
-                                </template>
-                                <v-date-picker v-model="dates" range :show-current='true' @input:>
-                                    <v-spacer></v-spacer>
-                                    <v-btn text color="primary" @click="menu = false">
-                                        Cancel
-                                    </v-btn>
-                                    <v-btn text color="primary" @click="$refs.menu.save(dates)">
-                                        OK
-                                    </v-btn>
-                                </v-date-picker>
-                            </v-menu>
-                        </v-col>
+                <v-text-field v-model="media" prepend-icon="mdi-book-open-variant" label="Media" single-line hide-details></v-text-field>
 
-                    </v-row>
-                </template>
+                <v-menu v-model="menu" :close-on-content-click="true" :nudge-right="40" transition="scale-transition" offset-y min-width="290px">
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-text-field v-model="date" label="Start Date" prepend-icon="mdi-calendar" readonly v-bind="attrs" v-on="on"></v-text-field>
+                    </template>
+                    <v-date-picker no-title v-model="date" :max='todaysDate' @click="$refs.menu.save(date)"></v-date-picker>
+                </v-menu>
             </v-flex>
 
             <v-spacer />
 
-            <v-flex xs12 md6>
-                <v-data-table sortable='false' disable-sort="true" v-model="selected" :headers="headers" :items="rows" :pagination.sync="pagination" item-key="name" :rows-per-page-items="[-1]" :search="filters" :custom-filter="customFilter" @click:row="popup=true" />
+            <v-flex align-center xs12 md6>
+                <v-data-table :mobile-breakpoint="0" :sort-by.sync="sortBy" :sort-desc.sync="sortDesc" :headers="headers" :items="topics" :search="search" @click:row="popup=true" />
+
             </v-flex>
             <Popup v-model="popup" />
             <v-col />
-
         </v-layout>
-        <v-spacer />
-        <v-flex>
-
-            <v-card>
-                <v-card-title primary-title>
-                    <h3>Filters log:</h3>
-                </v-card-title>
-                <ul>
-                    {{filters}}
-                </ul>
-            </v-card>
-        </v-flex>
-
     </template>
 </div>
 </template>
@@ -71,162 +45,117 @@ export default {
         Popup
     },
     computed: {
-        dateRangeText() {
-            return this.dates.join(' ~ ')
+        todaysDate() {
+            const today = new Date();
+            return this.formatDate(today);
+        },
+        dateRange() {
+            return this.todaysDate() + '-' + this.formatDate(this.start_date)
         },
     },
+
     data: () => ({
-        dates: ['', ''],
         popup: false,
 
+        date: '',
         menu: false,
-        filters: {
-            search: '',
-            media: '',
-            dates: ['', '']
-        },
-        media: ['ABC', 'The Guardian', 'The New York Times'],
-
-        pagination: {
-            sortBy: 'articles'
-        },
-        selected: [],
+        search: '',
+        medias: ['ABC', 'The Guardian', 'The New York Times'],
+        sortBy: 'articles',
+        sortDesc: true,
         headers: [{
-                text: 'Articles',
+                text: '# Articles',
                 value: 'articles',
                 width: "30%",
                 align: 'center',
-                sortable: false
             },
             {
                 text: 'Topic',
                 value: 'topic',
                 width: "100%",
                 align: 'center',
-                sortable: false,
+
             }
         ],
-        rows: [{
+        topics: [{
                 value: false,
-                articles: '5',
-                topic: 'Coronavirus'
+                articles: '908',
+                topic: 'Coronavirus',
+                date: '2020-10-08'
             },
             {
                 value: false,
-                articles: '4',
-                topic: 'U.S. Election'
+                articles: '1462',
+                topic: 'U.S. Election',
+                date: '2020-10-02'
             },
             {
                 value: false,
-                articles: '3',
+                articles: '826',
                 topic: 'Californian Bushfires',
+                date: '2020-10-2'
             },
             {
                 value: false,
-                articles: '2',
-                topic: 'New Zealand'
+                articles: '142',
+                topic: 'New Zealand',
+                date: '2020-10-17'
             },
             {
                 value: false,
-                articles: '1',
-                topic: 'Melbourne'
+                articles: '637',
+                topic: 'Melbourne',
+                date: '2020-10-12'
+            }, {
+                value: false,
+                articles: '2341',
+                topic: 'Scott Morrison',
+                date: '2020-10-14'
+            },
+            {
+                value: false,
+                articles: '1004',
+                topic: 'Iran',
+                date: '2020-10-03'
+            },
+            {
+                value: false,
+                articles: '837',
+                topic: 'Brexit',
+                date: '2020-10-05'
+            },
+            {
+                value: false,
+                articles: '463',
+                topic: 'Vaccine',
+                date: '2020-10-16'
+            },
+            {
+                value: false,
+                articles: '1349',
+                topic: 'ACT',
+                date: '2020-10-13'
             },
         ]
     }),
     methods: {
-        customFilter(items, filters, filter, headers) {
-            // Init the filter class.
-            const cf = new this.$MultiFilters(items, filters, filter, headers);
-            // Use regular function(),
-            // arrow functions does not allow context binding.
-            // Register the global standard filter.
-            cf.registerFilter('search', function (searchWord, items) {
-                if (searchWord.trim() === '') return items;
-                return items.filter(item => {
-                    return item.name.toLowerCase().includes(searchWord.toLowerCase());
-                }, searchWord);
-            });
-            cf.registerFilter('media', function (searchMedia, items) {
-                if (searchMedia.trim() === '') return items;
-                return items.filter(item => {
-                    return item.name.toLowerCase().includes(searchMedia.toLowerCase());
-                }, searchMedia);
-            });
-            // Use regular function(),
-            // arrow functions does not allow context binding.
-            // Register "start_date" filter.
-            cf.registerFilter('added_by', function (added_by, items) {
-                // If the filter has not been applied yet
-                // just return all available items.
-                if (added_by.trim() === '') return items;
-                // Compare each item added_by and just return the matching ones.
-                return items.filter(item => {
-                    return item.added_by.toLowerCase() === added_by.toLowerCase();
-                }, added_by);
-            });
-            // Use regular function(),
-            // arrow functions does not allow context binding.
-            // Register "end_date" filter.
-            cf.registerFilter('end_date', function (end_date, items) {
-                // If the filter has not been applied yet
-                // just return all available items.
-                if (end_date === null) return items;
-                // Compare each item end_date and just return the matching ones.
-                return items.filter(item => {
-                    return item.birth_date <= end_date;
-                }, end_date);
-            });
-            // Its time to run all created filters.
-            // Will be executed in the order thay were defined.
-            return cf.runFilters();
+        formatDate(date) {
+            let month = `${date.getMonth() + 1}`;
+            let day = `${date.getDate()}`;
+            const year = date.getFullYear();
+
+            if (month.length < 2) month = `0${month}`;
+            if (day.length < 2) day = `0${day}`;
+
+            return [year, month, day].join('-');
         },
-        /**
-         * Toggle selected items with the master checkbox.
-         */
-        toggleAll() {
-            if (this.selected.length) {
-                this.selected = [];
-            } else {
-                this.selected = this.rows.slice();
-            }
-        },
-        /**
-         * Column shorting.
-         *
-         * @param column
-         */
-        changeSort(column) {
-            if (this.pagination.sortBy === column) {
-                this.pagination.descending = !this.pagination.descending;
-            } else {
-                this.pagination.sortBy = column;
-                this.pagination.descending = false;
-            }
-        },
-        /**
-         * Handler when user input something at the "Filter" text field.
-         */
-        filterSearch(val) {
-            this.filters = this.$MultiFilters.updateFilters(this.filters, {
-                search: val
-            });
-        },
-        /**
-         * Handler when user  select some author at the "Author" select.
-         */
-        filterMedia(val) {
-            this.filters = this.$MultiFilters.updateFilters(this.filters, {
-                media: val
-            });
-        },
-        /**
-         * Handler when select a date on "From" date picker.
-         */
-        filterDates(val) {
-            this.filters = this.$MultiFilters.updateFilters(this.filters, {
-                dates: val
-            });
-        }
+
     }
-};
+}
 </script>
+
+<style>
+.v-data-table td {
+    font-size: 29px;
+}
+</style>
