4062a4397c6e92f50bf2369c551048d94925bd54
Liam Dale <liam.dale2@gmail.com>
2020-10-22 13:22:17 +1100

    saved trends popups

--- theNewsroom/front-end-client/src/views/Trends.vue
+++ theNewsroom/front-end-client/src/views/Trends.vue
@@ -12,10 +12,10 @@
                 <!-- Really, these filters wont actually filter the datatable, but rather will be used as input to our db query, thus changing the reuslts of the topics list returned by the database -->
 
                 <!-- For now this filters the datatable, really we want it to produce a popup with possible matches on 'enter', and selecting a match will produce the corresponding topic popup. This field ought to be in the same position of the page on both Topics and Trends, to show continuity -->
-                <v-card flat tile width='80%'>
+                <v-card flat tile width='95%'>
                     <v-text-field v-model="search" prepend-icon="mdi-magnify" label="Search for a topic" />
                 </v-card>
-                <v-card flat tile width='80%'>
+                <v-card flat tile width='95%'>
                     <v-menu ref="menu" v-model="menu" :close-on-content-click="false" :return-value.sync="date" transition="scale-transition" offset-y min-width="290px">
                         <template v-slot:activator="{ on, attrs }">
                             <v-text-field v-model="dateRange" label="Select time period" prepend-icon="mdi-calendar" readonly v-bind="attrs" v-on="on"></v-text-field>
@@ -28,7 +28,7 @@
                         </v-date-picker>
                     </v-menu>
                 </v-card>
-                <v-card flat tile width='90%'>
+                <v-card flat tile>
                     <v-list flat rounded dense>
                         <v-list-group value="true" color="none">
                             <template v-slot:activator>
@@ -65,6 +65,14 @@
                             </v-list-item-group>
                         </v-list-group>
                     </v-list>
+                    <v-card-actions>
+
+                        <v-btn rounded depressed width=49% @click='saveTrend'>Save Trend</v-btn>
+                        <v-btn rounded depressed width=49% @click='selectTrend'>Select Trend</v-btn>
+
+                    </v-card-actions>
+                    <Saved v-model="saved" />
+
                 </v-card>
             </v-flex>
             <v-spacer />
@@ -96,6 +104,7 @@ export default {
     },
 
     data: () => ({
+        saved: false,
         popup: false,
         start_date: '',
         end_date: '',
@@ -167,6 +176,10 @@ export default {
             'closeTopic'
         ]),
         open(topic) {
+            this.popup = true
+            this.openTopic(topic)
+        },
+        selectTrend(topic) {
             this.popup = true
             this.openTopic(topic)
         },
