b49a0cc03d25acfd63acb4be46c973c2fbc6eda7
Liam Dale <liam.dale2@gmail.com>
2020-11-09 16:00:26 +1100

    added containers to all pages to reduce clutter and improve reflexive sizing

--- theNewsroom/front-end-client/src/views/Trends.vue
+++ theNewsroom/front-end-client/src/views/Trends.vue
@@ -1,118 +1,119 @@
 <template>
 <div class="trends">
     <template>
+        <v-container fluid>
 
-        <v-layout wrap>
-            <v-spacer />
-            <v-flex xs10 md3>
-                <!-- Really, these filters wont actually filter the datatable, but rather will be used as input to our db query, thus changing the reuslts of the topics list returned by the database -->
+            <v-layout wrap>
+                <v-flex xs12 md3>
+                    <!-- Really, these filters wont actually filter the datatable, but rather will be used as input to our db query, thus changing the reuslts of the topics list returned by the database -->
 
-                <!-- For now this filters the datatable, really we want it to produce a popup with possible matches on 'enter', and selecting a match will produce the corresponding topic popup. This field ought to be in the same position of the page on both Topics and Trends, to show continuity -->
-                <v-card flat tile width='100%'>
-                    <v-list ripple=false expand flat rounded dense>
+                    <!-- For now this filters the datatable, really we want it to produce a popup with possible matches on 'enter', and selecting a match will produce the corresponding topic popup. This field ought to be in the same position of the page on both Topics and Trends, to show continuity -->
+                    <v-card flat tile width='100%'>
+                        <v-list ripple=false expand flat rounded dense>
 
-                        <!-- Search, calendar and media are subgroups in a the group Filters, allowing us to easily modify this entire list as a single element -->
-                        <v-list-group value="true" color="none">
+                            <!-- Search, calendar and media are subgroups in a the group Filters, allowing us to easily modify this entire list as a single element -->
+                            <v-list-group value="true" color="none">
 
-                            <template v-slot:activator>
-                                <v-list-item-content>
-                                    <v-list-item-title class='font-weight-light list-title'>Filters</v-list-item-title>
-                                </v-list-item-content>
-                            </template>
-                            <!-- Search -->
-                            <v-list-item>
-                                <v-text-field @keyup.enter.native="searchTopic" dense rounded filled v-model="keyword" append-icon="mdi-magnify" label="Search for a topic" single-line hide-details />
-                                <Search v-model="search" />
-                            </v-list-item>
-                            <!-- Calendar -->
-                            <v-list-item>
-                                <v-menu ref="menu" v-model="menu" :close-on-content-click="false" :return-value.sync="date" transition="scale-transition" offset-y min-width="290px">
-                                    <template v-slot:activator="{ on, attrs }">
-                                        <v-text-field dense filled rounded v-model="dateRange" label="Select time period" append-icon="mdi-calendar" single-line hide-details readonly v-bind="attrs" v-on="on" />
-                                    </template>
-                                    <v-date-picker v-model="dates" :max='todaysDate' range no-title scrollable>
-                                        <v-spacer />
-                                        <v-btn text color="primary" @click="saveDates">
-                                            OK
-                                        </v-btn>
-                                    </v-date-picker>
-                                </v-menu>
-                            </v-list-item>
-                        </v-list-group>
-                        <!-- Selected, Related and Saved topics are also presented as groups in the List, allowing us to open and close them easily to show their internal components. Selected is set to true so that it is shown automatically, whilst the others are closed by default -->
-                        <v-list-group value="true" color="none">
-                            <template v-slot:activator>
-                                <v-list-item-content>
-                                    <v-list-item-title class='font-weight-light list-title'>Selected Topics</v-list-item-title>
-                                </v-list-item-content>
-                            </template>
-                            <v-list-item-group value="true" color="none">
-                                <v-list-item class='item' v-for="item in getSelected" :key="item">
-                                    <v-list-item-title @click='open(item)' v-text="item.name" />
-                                    <v-btn icon @click='removeSelected(item)'>
-                                        <v-icon color="grey lighten-1">mdi-minus-circle</v-icon>
-                                    </v-btn>
-                                </v-list-item>
-                            </v-list-item-group>
-                        </v-list-group>
-                        <v-list-group color="none">
-                            <template v-slot:activator>
-                                <v-list-item-content>
-                                    <v-list-item-title class='font-weight-light list-title'>Related Topics</v-list-item-title>
-                                </v-list-item-content>
-                            </template>
-                            <v-list-item-group color="none">
-                                <v-list-item class='item' v-for="item in getRelatedTopics" :key="item">
-                                    <v-list-item-title @click='open(item)' v-text="item.name" />
-                                    <v-btn icon @click='addSelected(item)'>
-                                        <v-icon color="grey lighten-1">mdi-plus-circle</v-icon>
-                                    </v-btn>
-                                </v-list-item>
-                            </v-list-item-group>
-                        </v-list-group>
-                        <template v-if="!$auth.loading & $auth.isAuthenticated">
-                            <v-list-group color="none">
                                 <template v-slot:activator>
                                     <v-list-item-content>
-                                        <v-list-item-title class='font-weight-light list-title'>Saved Trends</v-list-item-title>
+                                        <v-list-item-title class='font-weight-light list-title'>Filters</v-list-item-title>
                                     </v-list-item-content>
                                 </template>
-                                <v-list-item-group color="none">
-                                    <v-list-item class='item' v-for="config in getSaved" :key="config" @click="setSelected(config.topics)">
-                                        <v-list-item-title v-text=" config.title" />
+                                <!-- Search -->
+                                <v-list-item>
+                                    <v-text-field @keyup.enter.native="searchTopic" dense rounded filled v-model="keyword" append-icon="mdi-magnify" label="Search for a topic" single-line hide-details />
+                                    <Search v-model="search" />
+                                </v-list-item>
+                                <!-- Calendar -->
+                                <v-list-item>
+                                    <v-menu ref="menu" v-model="menu" :close-on-content-click="false" :return-value.sync="date" transition="scale-transition" offset-y min-width="290px">
+                                        <template v-slot:activator="{ on, attrs }">
+                                            <v-text-field dense filled rounded v-model="dateRange" label="Select time period" append-icon="mdi-calendar" single-line hide-details readonly v-bind="attrs" v-on="on" />
+                                        </template>
+                                        <v-date-picker v-model="dates" :max='todaysDate' range no-title scrollable>
+                                            <v-spacer />
+                                            <v-btn text color="primary" @click="saveDates">
+                                                OK
+                                            </v-btn>
+                                        </v-date-picker>
+                                    </v-menu>
+                                </v-list-item>
+                            </v-list-group>
+                            <!-- Selected, Related and Saved topics are also presented as groups in the List, allowing us to open and close them easily to show their internal components. Selected is set to true so that it is shown automatically, whilst the others are closed by default -->
+                            <v-list-group value="true" color="none">
+                                <template v-slot:activator>
+                                    <v-list-item-content>
+                                        <v-list-item-title class='font-weight-light list-title'>Selected Topics</v-list-item-title>
+                                    </v-list-item-content>
+                                </template>
+                                <v-list-item-group value="true" color="none">
+                                    <v-list-item class='item' v-for="item in getSelected" :key="item">
+                                        <v-list-item-title @click='open(item)' v-text="item.name" />
+                                        <v-btn icon @click='removeSelected(item)'>
+                                            <v-icon color="grey lighten-1">mdi-minus-circle</v-icon>
+                                        </v-btn>
                                     </v-list-item>
                                 </v-list-item-group>
                             </v-list-group>
+                            <v-list-group color="none">
+                                <template v-slot:activator>
+                                    <v-list-item-content>
+                                        <v-list-item-title class='font-weight-light list-title'>Related Topics</v-list-item-title>
+                                    </v-list-item-content>
+                                </template>
+                                <v-list-item-group color="none">
+                                    <v-list-item class='item' v-for="item in getRelatedTopics" :key="item">
+                                        <v-list-item-title @click='open(item)' v-text="item.name" />
+                                        <v-btn icon @click='addSelected(item)'>
+                                            <v-icon color="grey lighten-1">mdi-plus-circle</v-icon>
+                                        </v-btn>
+                                    </v-list-item>
+                                </v-list-item-group>
+                            </v-list-group>
+                            <template v-if="!$auth.loading & $auth.isAuthenticated">
+                                <v-list-group color="none">
+                                    <template v-slot:activator>
+                                        <v-list-item-content>
+                                            <v-list-item-title class='font-weight-light list-title'>Saved Trends</v-list-item-title>
+                                        </v-list-item-content>
+                                    </template>
+                                    <v-list-item-group color="none">
+                                        <v-list-item class='item' v-for="config in getSaved" :key="config" @click="setSelected(config.topics)">
+                                            <v-list-item-title v-text=" config.title" />
+                                        </v-list-item>
+                                    </v-list-item-group>
+                                </v-list-group>
 
-                        </template>
+                            </template>
 
-                        <v-list-item>
-                            <v-spacer />
-                            <SaveTrend v-if="!$auth.loading & $auth.isAuthenticated" />
-                            <v-btn rounded depressed @click="emptySelected()">
-                                Clear
-                            </v-btn>
-                            <HelpTrends />
+                            <v-list-item>
+                                <v-spacer />
+                                <SaveTrend v-if="!$auth.loading & $auth.isAuthenticated" />
+                                <v-btn rounded depressed @click="emptySelected()">
+                                    Clear
+                                </v-btn>
+                                <HelpTrends />
 
-                        </v-list-item>
-                    </v-list>
-                </v-card>
-            </v-flex>
-            <v-spacer />
+                            </v-list-item>
+                        </v-list>
+                    </v-card>
+                </v-flex>
+                <v-spacer />
 
-            <v-flex align-center xs12 md6>
-                <template>
-                    <div>
-                        <apexchart type="line" :options="options" :series="series"></apexchart>
-                    </div>
-                </template>
-            </v-flex>
+                <v-flex align-center xs12 md8>
+                    <template>
+                        <div>
+                            <apexchart type="line" :options="options" :series="series"></apexchart>
+                        </div>
+                    </template>
+                </v-flex>
 
-            <!-- Same as on Home and Topics, this is only shown when popup = true and is closed when popup = false -->
-            <Popup v-model="popup" />
+                <!-- Same as on Home and Topics, this is only shown when popup = true and is closed when popup = false -->
+                <Popup v-model="popup" />
 
-            <v-col />
-        </v-layout>
+                <v-col />
+            </v-layout>
+        </v-container>
     </template>
 </div>
 </template>
@@ -152,10 +153,6 @@ export default {
             colors: ['#E91E63', '#2E93fA', '#66DA26', '#FF9800'],
             xaxis: {
                 type: 'datetime'
-            },
-            title: {
-                text: 'Topic Trends',
-                align: 'left'
             },
 
         },
