8097060d724528f4b583d928580aeea29d6815c4
Jonathan Chan <jonowschan@gmail.com>
2020-11-11 20:13:20 +1100

    Removed data from 02-manually-generated-data.sql

--- theNewsroom/API/postgres_db/init/02-manually-generated-data.sql
+++
@@ -1,7 +0,0 @@
--- DATA INSERTING
-INSERT INTO NewsCollectorInfo.PublicNewsAPISources(name, endpoint_URL) VALUES ('GuardiaAPI', 'http://guardian.com');
-INSERT INTO NewsCollectorInfo.PublicNewsAPISources(name, endpoint_URL) VALUES ('NewsAPI', 'http://newsAPI.io');
-
-INSERT INTO NewsCollectorInfo.MediaOutlets(name) VALUES ('Guardian');
-INSERT INTO NewsCollectorInfo.MediaOutlets(name) VALUES ('Australian');
-INSERT INTO NewsCollectorInfo.MediaOutlets(name) VALUES ('The Sydney Morning Herald');
