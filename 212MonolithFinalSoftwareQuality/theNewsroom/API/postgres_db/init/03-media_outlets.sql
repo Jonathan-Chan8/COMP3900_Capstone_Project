b9d50b854653a7f6aab1b2b46306a9442aa647e3
Liam Dale <liam.dale2@gmail.com>
2020-11-16 23:54:15 +1100

    FINAL SUBMISSION ZIP ONLY - ORIGINAL AVAILABLE AT MASTER

--- 212MonolithFinalSoftwareQuality/theNewsroom/API/postgres_db/init/03-media_outlets.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO thenewsroom_database.NewsCollectorInfo.mediaoutlets (name) values ('Guardian');
-INSERT INTO thenewsroom_database.NewsCollectorInfo.mediaoutlets (name) values ('Australian');
